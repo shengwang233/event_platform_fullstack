7f49ca22337f46bf1f77b847dc4d232607281f0f
Sheng <wang283@sheridancollege.ca>
2025-04-09 18:12:28 -0400

    edit usertype

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -72,7 +72,7 @@ export async function POST(req: Request) {
       firstName: first_name!,
       lastName: last_name!,
       photo: image_url,
-      userType,
+      userType: userType,
     };
 
     const newUser = await createUser(user);
